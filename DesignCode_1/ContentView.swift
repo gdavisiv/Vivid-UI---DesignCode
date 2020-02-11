cf5452563d0411e1f553575995568927d5410b16
gdavisiv <gdavisiv@gmail.com>
2020-02-10 20:05:39 -0500

    Code updates

--- DesignCode_1/ContentView.swift
+++ DesignCode_1/ContentView.swift
@@ -10,30 +10,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            HStack{
-                VStack(alignment: .leading) {
-                    Text("UI Design")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                    Text("Certificate")
-                        .foregroundColor(Color("accent"))
+        ZStack{
+            VStack{
+            Spacer()
+            }
+            .frame(width:300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x:0, y:-20)
+        
+            VStack{
+                HStack{
+                    VStack(alignment: .leading) {
+                        Text("UI Design")
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Text("Certificate")
+                            .foregroundColor(Color("accent"))
                 }
-                    Spacer()
-                    Image("Logo1")
+                Spacer()
+                        Image("Logo1")
             }
             .padding(.horizontal, 20)
             .padding(.top, 20)
-            Spacer()
-            Image("Card1")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+                Spacer()
+                        Image("Card1")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
         }
         .frame(width:340.0, height: 220.0)
         .background(Color.black)
         .cornerRadius(20)
         .shadow(radius: 20)
+        }
     }
 }
 
