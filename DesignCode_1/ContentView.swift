7a4ee17d05b5194337612c7edb0c2da22af04cca
gdavisiv <gdavisiv@gmail.com>
2020-02-10 20:58:27 -0500

    UI Updates

--- DesignCode_1/ContentView.swift
+++ DesignCode_1/ContentView.swift
@@ -20,7 +20,30 @@ struct ContentView: View {
             .shadow(radius: 20)
             .offset(x:0, y:-20)
         
+            CardView()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        ZStack{
             VStack{
+                Spacer()
+            }
+            .frame(width:300, height: 220)
+            .background(Color.blue)
+            .cornerRadius(20)
+            .shadow(radius: 20)
+            .offset(x:0, y:-20)
+            
+            VStack {
                 HStack{
                     VStack(alignment: .leading) {
                         Text("UI Design")
@@ -29,27 +52,21 @@ struct ContentView: View {
                             .foregroundColor(.white)
                         Text("Certificate")
                             .foregroundColor(Color("accent"))
+                    }
+                    Spacer()
+                    Image("Logo1")
                 }
+                .padding(.horizontal, 20)
+                .padding(.top, 20)
                 Spacer()
-                        Image("Logo1")
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
             }
-            .padding(.horizontal, 20)
-            .padding(.top, 20)
-                Spacer()
-                        Image("Card1")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
+            .frame(width:340.0, height: 220.0)
+            .background(Color.black)
+            .cornerRadius(20)
+            .shadow(radius: 20)
         }
-        .frame(width:340.0, height: 220.0)
-        .background(Color.black)
-        .cornerRadius(20)
-        .shadow(radius: 20)
-        }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
