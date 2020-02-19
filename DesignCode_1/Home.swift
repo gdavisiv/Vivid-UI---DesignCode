b3314df310d8f5e300886ffdcf031696876b4af9
gdavisiv <gdavisiv@gmail.com>
2020-02-18 23:45:39 -0500

    Completed adding Animation/3D Effects for UX

--- DesignCode_1/Home.swift
+++ DesignCode_1/Home.swift
@@ -14,6 +14,9 @@ struct Home: View {
     
     var body: some View {
         ZStack{
+            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                .edgesIgnoringSafeArea(.all)
+            
             VStack {
                 HStack {
                     Text("Watching")
@@ -34,6 +37,17 @@ struct Home: View {
                 
                 Spacer()
             }
+            .padding(.top, 35)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .offset(y: showProfile ? -450 : 0)
+            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+            .scaleEffect(showProfile ? 0.9 : 1)
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            .edgesIgnoringSafeArea(.all)
+            
+            
             MenuView()
                 .offset(y: showProfile ? 0 : 600)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
