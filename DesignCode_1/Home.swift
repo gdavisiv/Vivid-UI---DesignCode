7e5ddd8fa8183363d8254477b2221d022dffdc7b
gdavisiv <gdavisiv@gmail.com>
2020-02-19 23:14:21 -0500

    Add Drag Animation and Tap interaction to the Background

--- DesignCode_1/Home.swift
+++ DesignCode_1/Home.swift
@@ -16,6 +16,9 @@ struct Home: View {
     
     @State var showProfile = false
     
+    //Created a state to store the DRAG gesture translation value
+    @State var viewState = CGSize.zero
+    
     var body: some View {
         ZStack{
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
@@ -46,15 +49,33 @@ struct Home: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: showProfile ? -450 : 0)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0), axis: (x: 10.0, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
             
             
             MenuView()
-                .offset(y: showProfile ? 0 : 600)
+                //This allows you to not see the menu, but still make it interactive
+                .background(Color.black.opacity(0.001))
+                .offset(y: showProfile ? 0 : 1000)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showProfile.toggle()
+                }
+                .gesture(
+                    DragGesture().onChanged { value in
+                        self.viewState = value.translation
+                    }
+                    .onEnded { value in
+                        if self.viewState.height > 50 {
+                            self.showProfile = false
+                        }
+                        self.viewState = .zero
+                        
+                    }
+                )
         }
     }
 }
