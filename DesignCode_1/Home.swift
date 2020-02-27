4be19c6bfc2c8cfb3c0bba7af3317e43333e7c56
gdavisiv <gdavisiv@gmail.com>
2020-02-26 19:20:03 -0500

    Updated commenting for the new code I added previously

--- DesignCode_1/Home.swift
+++ DesignCode_1/Home.swift
@@ -49,6 +49,8 @@ struct Home: View {
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             .offset(y: showProfile ? -450 : 0)
+            //Added Double(viewState.height / 10) - 10 : 0) so that you are dividing 50/10=5 so a softer transition when dragging
+            //the bottom menu up or down
             .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 10) - 10 : 0), axis: (x: 10.0, y: 0, z: 0))
             .scaleEffect(showProfile ? 0.9 : 1)
             .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
@@ -59,16 +61,21 @@ struct Home: View {
                 //This allows you to not see the menu, but still make it interactive
                 .background(Color.black.opacity(0.001))
                 .offset(y: showProfile ? 0 : 1000)
+                //Applies the offset to the menu so that it animates
                 .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                //Add this so you can press the profile and make it interactive
                 .onTapGesture {
                     self.showProfile.toggle()
                 }
+                //Add a new drag gesture to the menu view.  If the Y position is
+                //higher than a certain number during the drag procvess it will reset the Menu position
                 .gesture(
                     DragGesture().onChanged { value in
                         self.viewState = value.translation
                     }
                     .onEnded { value in
+                        //this will dismiss the menu past a height of 50 on y-axis
                         if self.viewState.height > 50 {
                             self.showProfile = false
                         }
