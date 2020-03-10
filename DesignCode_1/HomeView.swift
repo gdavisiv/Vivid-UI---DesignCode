5fad9b625af9b1b7ec01bc5b4f79989a13daff93
gdavisiv <gdavisiv@gmail.com>
2020-03-10 19:10:38 -0400

    Completed the addition of Scrolling View and Repeating Info Cards/Content

--- DesignCode_1/HomeView.swift
+++ DesignCode_1/HomeView.swift
@@ -25,14 +25,22 @@ struct HomeView: View {
                 
             }
             .padding(.horizontal)
+            .padding(.leading, 14)
             .padding(.top, 30)
             
-            HStack {
-                Text("Prototype Design")
-                    .font(.system(size: 24, weight: .bold))
-                    .frame(width: 160, alignment: .leading)
-                Image("Logo1")
-            }
+            //Created Section View now we need to create a repeat for each of these elements and then we
+            //make tha scrollview horizontal and then place the image in the HStack
+            //turn off the scroll indicator
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30){
+                    ForEach(0 ..< 5) { item in
+                        SectionView()
+                        }
+                    }
+                    //Adds extra padding so the scroll view doesn't clip the dropshadow
+                    .padding(30)
+                    .padding(.bottom, 30)
+                }
             
             Spacer()
         }
@@ -46,4 +54,31 @@ struct HomeView_Previews: PreviewProvider {
         //we don't have a state to use.  So instead we set .constant(false)
         HomeView(showProfile: .constant(false))
     }
+}
+
+struct SectionView: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype Design")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                Spacer()
+                Image("Logo1")
+            }
+            Text("15 Sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+            
+        }
+        .padding(.top, 20)
+        .padding(.horizontal, 20)
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)    }
 }
