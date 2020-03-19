5a85240d358ddd663273a05523a587b5402cf0d6
gdavisiv <gdavisiv@gmail.com>
2020-03-19 17:37:40 -0400

    Adding and creating variables to make a customizable and animated Ring

--- DesignCode_1/RingView.swift
+++ DesignCode_1/RingView.swift
@@ -9,6 +9,15 @@
 import SwiftUI
 
 struct RingView: View {
+    
+    //setting variables so that we can customize our component
+    var color1 = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    var color2 = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    //.frame uses CGFloat and not default Int
+    var width: CGFloat = 44
+    var height: CGFloat = 44
+    var percent = 88
+    
     var body: some View {
         ZStack {
             //Positioning is very important to get this to work properly so pay close attention!!
@@ -16,14 +25,14 @@ struct RingView: View {
             Circle()
                 .stroke(Color.black.opacity(0.1), style:
                     StrokeStyle(lineWidth: 5))
-                .frame(width: 44, height: 44)
+                .frame(width: width, height: height)
             //This creates the main color circle
             Circle()
                 //The trim is a bit counter-intuitive, the 0.2 = 80%, and starts around x=10, y =0
                 .trim(from: 0.2, to: 1)
                 .stroke(
-                    LinearGradient(gradient: Gradient(colors:[Color(#colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1)),
-                            Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))]), startPoint: .topTrailing,
+                    LinearGradient(gradient: Gradient(colors:[Color(color1),
+                            Color(color2)]), startPoint: .topTrailing,
                             endPoint: .bottomLeading),
                         style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0],
                             dashPhase: 0)
@@ -32,8 +41,8 @@ struct RingView: View {
             //adding the rotation will move the start point to the bottom
             .rotationEffect(Angle(degrees: 90))
             .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
-            .frame(width: 44, height: 44)
-            .shadow(color: Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).opacity(0.1), radius: 3, x: 0, y: 3)
+            .frame(width: width, height: height)
+                .shadow(color: Color(color2).opacity(0.1), radius: 3, x: 0, y: 3)
             
             //Adds text in the middle of the status circle
             //positioning is important
