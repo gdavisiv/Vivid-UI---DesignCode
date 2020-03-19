b0f6b4f984ef2eaf42ce88e48ec195b36ef75b42
gdavisiv <gdavisiv@gmail.com>
2020-03-18 20:04:33 -0400

    Creating the Bottom Tab bar for easy Navigation between menu items

---
+++ DesignCode_1/TabBar.swift
@@ -0,0 +1,30 @@
+//
+//  TabBar.swift
+//  DesignCode_1
+//
+//  Created by user163072 on 3/18/20.
+//  Copyright © 2020 user163072. All rights reserved.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        TabView {
+            Home().tabItem {
+                Image(systemName: "play.circle.fill")
+                Text("Home")
+            }
+            ContentView().tabItem {
+                Image(systemName: <#T##String#>)
+            }
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+struct TabBar_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBar()
+    }
+}
