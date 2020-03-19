513c085fe778d04dd3b4b7ae5b8d7df40c719fdf
gdavisiv <gdavisiv@gmail.com>
2020-03-19 18:08:22 -0400

    Completed creation of the Ring Component along with making it customizable

--- DesignCode_1/RingView.swift
+++ DesignCode_1/RingView.swift
@@ -11,30 +11,38 @@ import SwiftUI
 struct RingView: View {
     
     //setting variables so that we can customize our component
-    var color1 = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-    var color2 = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    var color1 = #colorLiteral(red: 0.3176470697, green: 0.07450980693, blue: 0.02745098062, alpha: 1)
+    var color2 = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
     //.frame uses CGFloat and not default Int
-    var width: CGFloat = 44
-    var height: CGFloat = 44
-    var percent = 88
+    var width: CGFloat = 88
+    var height: CGFloat = 88
+    var percent: CGFloat = 25
     
     var body: some View {
-        ZStack {
+        //Add the variable inside the body because it will not intialize with the
+        //correct inputs outside the body
+        //Also add return ZStack once you add the let multiplier
+        let multiplier = width / 44
+        
+        //Added a  "1 -" because it would start from the left instead of the right
+        let progress = 1 - (percent / 100)
+        
+        return ZStack {
             //Positioning is very important to get this to work properly so pay close attention!!
             //This creates the grey inactive ring in the background
             Circle()
                 .stroke(Color.black.opacity(0.1), style:
-                    StrokeStyle(lineWidth: 5))
+                    StrokeStyle(lineWidth: 5 * multiplier))
                 .frame(width: width, height: height)
             //This creates the main color circle
             Circle()
                 //The trim is a bit counter-intuitive, the 0.2 = 80%, and starts around x=10, y =0
-                .trim(from: 0.2, to: 1)
+                .trim(from: progress, to: 1)
                 .stroke(
                     LinearGradient(gradient: Gradient(colors:[Color(color1),
                             Color(color2)]), startPoint: .topTrailing,
                             endPoint: .bottomLeading),
-                        style: StrokeStyle(lineWidth: 5, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0],
+                        style: StrokeStyle(lineWidth: 5 * multiplier, lineCap: .round, lineJoin: .round, miterLimit: .infinity, dash: [20, 0],
                             dashPhase: 0)
             )
             //Adding customization for starting point, and rotation/scale
@@ -42,12 +50,13 @@ struct RingView: View {
             .rotationEffect(Angle(degrees: 90))
             .rotation3DEffect(Angle(degrees: 180), axis: (x: 1, y: 0, z: 0))
             .frame(width: width, height: height)
-                .shadow(color: Color(color2).opacity(0.1), radius: 3, x: 0, y: 3)
+            .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
             
             //Adds text in the middle of the status circle
             //positioning is important
-            Text("82%")
-                .font(.subheadline)
+            //CGFloat uses extra decimals so we convert it to an Int instead
+            Text("\(Int(percent))%")
+                .font(.system(size: 14 * multiplier))
                 .fontWeight(.bold)
         }
     }
