fc07f6c1a21f40fbc9f12a1a98890b20f639befb
gdavisiv <gdavisiv@gmail.com>
2020-03-20 16:52:03 -0400

    Implementing custom modifiers for drop shadow, shapes, fonts, etc to make editing/creation easier

---
+++ DesignCode_1/Modifiers.swift
@@ -0,0 +1,31 @@
+//
+//  Modifiers.swift
+//  DesignCode_1
+//
+//  Created by GdavisIV on 3/20/20.
+//  Copyright © 2020 George Davis IV. All rights reserved.
+//
+
+import SwiftUI
+
+//Creating the Components of modifiers similar to have a style guide in CSS
+struct ShadowModifier: ViewModifier {
+    //Return some view, and use content, and apply the modifiers to content
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
+            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+        
+    }
+}
+
+struct FontModifier: ViewModifier {
+    //Font modifier has a .body set by default we that can not be changed.
+    //so we can make the .body customizable by making .style variable
+    var style: Font.TextStyle = .body
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.body, design: .rounded))
+    }
+}
