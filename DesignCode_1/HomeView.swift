07fcdd8867c004ded7ff7518e6455334ec670280
gdavisiv <gdavisiv@gmail.com>
2020-03-20 12:54:16 -0400

    Completed Adding Animation HomeView & ContentView

--- DesignCode_1/HomeView.swift
+++ DesignCode_1/HomeView.swift
@@ -52,6 +52,8 @@ struct HomeView: View {
             .padding(.leading, 14)
             .padding(.top, 30)
             
+            //Added this Ring Indicator to the top of the HomeView
+            //This RingView will not be animated an instead .constant(true)
             HStack {
                 RingView(color1: #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), color2: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), width: 44, height: 44, percent: 68, show: .constant(true))
                 VStack{
