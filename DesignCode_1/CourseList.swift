07ce63edd5637e3b34f227baa6e8db961108cac2
gdavisiv <gdavisiv@gmail.com>
2020-03-28 22:56:15 -0400

    Updated CourseView Struct with Course Data Array info

--- DesignCode_1/CourseList.swift
+++ DesignCode_1/CourseList.swift
@@ -11,9 +11,10 @@ import SwiftUI
 struct CourseList: View {
     //State a starts from Courselist and pass this to each instance of CourseView
     //Bound to a state show at the root level
-    @State var show = false
+    //@State var show = false
     //This allows a different state for each card
-    @State var show2 = false
+    //@State var show2 = false
+    //Manual States no Longer needed with implementation of Data model
     //This new state will allow us to use the course Array Data created
     @State var courses = courseData
     
@@ -21,27 +22,35 @@ struct CourseList: View {
         ScrollView {
             VStack(spacing: 30) {
                 //This will repeat the demo record 5 times (***PAY ATTENTION TO SPACING!!!***)
-                ForEach(0 ..< 5) { item in
+                //instead of looping through the courses we are going to get the index value for the courses
+                //This will provide the index
+                ForEach(courses.indices, id: \.self) { index in
                 //Don't need the code below now that we are implementing Course Data from an Array
                 //CourseView(show: $show)
                 //Use the gemetry reader to detect the scroll positions of every single card, and use those positions
                 //to create a gap between the two cardsdetermine the spacing over every single card
                     GeometryReader { geometry in
                         //We will need to use self since we are inside Geometry Reader
-                        CourseView(show: self.$show2)
+                        //CourseView(show: self.$show2)
+                        //To use the Courses Array we call the following
+                        CourseView(show: self.$courses[index].show, course: self.courses[index])
                             //If self.show2 this is in fullscreen it will use minY position (between the two cards) else don't change anything
                             //minY is the position of the top of the second card, and we use negative minY to fill the gap left
                             //by the top card as it receeds, else don't change anything
-                            .offset(y: self.show2 ? -geometry.frame(in: .global).minY: 0)
+                            .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY: 0)
                     }
+                        //Since I added the for each, have to update self.show2 with self first with self.courses[index].show
                     //this should fix the height of the container at fullscreen
                     //If show2 is true, set it to scree.height, otherwise set it to 280
-                    .frame(height: self.show2 ? screen.height : 280)
+                    .frame(height: self.courses[index].show ? screen.height : 280)
                     //This will move the second card, because the card is set to infinity, and it is centered in the vstack with width - 60
-                    .frame(maxWidth: self.show2 ? .infinity : screen.width - 60)
+                    .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
                 }
             }
             .frame(width: screen.width)
+            //This fixes the sudden animation transition
+            .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            
         }
     }
 }
@@ -58,6 +67,8 @@ struct CourseView: View {
     //To add a second card we need to change the state from State to a Binding
     //@State var show = false
     @Binding var show: Bool
+    //State applying the Data Array
+    var course: Course
     
     var body: some View {
         //Added ZStack to add content behind the created card on Z axis
@@ -82,16 +93,22 @@ struct CourseView: View {
             VStack {
                 HStack(alignment: .top) {
                     VStack(alignment: .leading, spacing: 8.0) {
-                        Text("iOS Swift UI Advanced")
+                        //Update to new Code
+                        //Text("iOS Swift UI Advanced")
+                        Text(course.title)
                             .font(.system(size: 24, weight: .bold))
                             .foregroundColor(.white)
-                        Text("20 Sections")
+                        //Update to new Data Array
+                        //Text("20 Sections")
+                        Text(course.subtitle)
                             .foregroundColor(Color.white.opacity(0.7))
                         }
                         Spacer()
                         //ImageLiteral to easily select an image in your assets folder!
                     ZStack {
-                        Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                        //Update to new Data Array
+                        //Image(uiImage: #imageLiteral(resourceName: "Logo1"))
+                        Image(uiImage: course.logo)
                             //This will set the opacity to 0 otherwise set it to 1 or show
                             //This will make the logo disappear in fullscreen mode
                             .opacity(show ? 0 : 1)
@@ -108,7 +125,9 @@ struct CourseView: View {
                         }
                     }
                     Spacer()
-                    Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                    //Update to new Datay Array
+                    //Image(uiImage: #imageLiteral(resourceName: "Card2"))
+                    Image(uiImage: course.image)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(maxWidth: .infinity)
@@ -117,16 +136,20 @@ struct CourseView: View {
                 //Add animation to the padding modifier so that it updates from CArd to Fullscreen Mode
                 .padding(show ? 30 : 20)
                 //This adds extra padding at the very top so that it does not intefer with the top Status Menu
-                .padding(.top, show ? 30 : 0)
+                //.padding(.top, show ? 30 : 0)
                 //This following code will add the transition animation
                 //Adding a Tap Gesture : Just need to update the frame, show screen.width otherwise show width - 60; ""
                 //.frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
                 //There is a better way to write the same code as shown above!  This will ensure that the card
                 //maximizes and takes up the entire screen
                 .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
-                .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                //Update to new Array Data
+                //.background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
+                .background(Color(course.color))
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-                .shadow(color: Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).opacity(0.3), radius: 20, x: 0, y: 20)
+                //Update to new Array Data
+                //.shadow(color: Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).opacity(0.3), radius: 20, x: 0, y: 20)
+                .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
                 //Have to move the animation from this location to parent container because we want it to effect
                 //the text and card at the same time, instead of just the card
                 //.animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
@@ -161,6 +184,6 @@ struct Course: Identifiable {
 //Each array is using a Data model, and set show to false by default
 var courseData = [
     Course(title: "Protyping for Designing in SwiftUI", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Background1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1), show: false),
-    Course(title: "Advanced Designing in SwiftUI", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Background1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.5725490451, green: 0, blue: 0.2313725501, alpha: 1), show: false),
-    Course(title: "UI Design for Developers", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Card5"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1), show: false)
+    Course(title: "Advanced Designing in SwiftUI", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Background1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), show: false),
+    Course(title: "UI Design for Developers", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Card5"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), show: false)
 ]
