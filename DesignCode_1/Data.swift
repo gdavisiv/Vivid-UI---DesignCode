d6cd4d89aa83f5b2f79547c56a83c829032bc4e2
George Davis IV <gdavisiv@gmail.com>
2020-04-05 21:55:11 -0400

    Adding Comment Code cover complete explanation of how to fetch JSON data, and populate a list view

--- DesignCode_1/Data.swift
+++ DesignCode_1/Data.swift
@@ -5,22 +5,31 @@
 //  Created by George Davis IV on 4/5/20.
 //  Copyright © 2020 George Davis IV. All rights reserved.
 //
+//Using JSON data from the following website :
+//https://jsonplaceholder.typicode.com/
 
 import SwiftUI
 
+//Create a new Data Model, of type codable so that we can use JSON format, and identifiable so we can
+//loop through the data and make it easy to read in SwiftUI
 struct Post: Codable, Identifiable {
     let id = UUID()
     var title: String
     var body: String
 }
 
+//Create an API class
 class Api {
     func getPost(completion: @escaping ([Post]) -> ()) {
+        //Needed to create an API call, and then input the website link of the JSON format of the Data
+        //Using Guard we can use it tso that if it is not a URL otherwise it will stop running the code
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
         //Come back to FULL understand the importance of this!! and Comment it!
-        //This is about optionals and working with APIs
+        //Make the API Call : The completion handler allows us to set the names of the data that we get back from the
+        //response.  For the rest of the responses we will set as '_' and that simply means we will not use it for the moment
         URLSession.shared.dataTask(with: url) { (data, _, _) in
+            //We will now decode the JSON in Swift.  It will decode and use the data model we created
             let posts = try! JSONDecoder().decode([Post].self, from: data!)
             
             DispatchQueue.main.async {
