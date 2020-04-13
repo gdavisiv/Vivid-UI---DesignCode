81d1fbfd840e8f1bec1639da4bde9113fed07f90
George Davis IV <GDavisIV@gmail.com>
2020-04-13 11:54:17 -0400

    Implementing API data into CourseData

--- DesignCode_1/CourseStore.swift
+++ DesignCode_1/CourseStore.swift
@@ -16,11 +16,15 @@ import Contentful
 let client = Client(spaceId: "q9yfx3ed18xx", accessToken: "CLoS8H84d0SYq9-zZjy1uFU3LKKHQ4psQw6TojBtys8")
 
 //How to use their API : https://github.com/contentful/contentful.swift/
-func getArray() {
+//Now that we're receving data, we going to use that data to fill our UI :
+//create a completion handler, '@escaping([entry]) -> () so when the call is successful and we're reciving the data
+//we don't want to print anymore and we want to send back the array that we are receiving so we can use
+//that in the UI
+func getArray(id: String, completion: @escaping([Entry]) -> ()) {
     //The EntryDecodable protocol allows you to define a mapping between your content
     //types and your Swift classes that entries will be serialized to.
     //Couse is simply the content model
-    let query = Query.where(contentTypeId: "course")
+    let query = Query.where(contentTypeId: id)
     
     //To make the API call use client which was configured above, entry is the content model from contentful
     //
