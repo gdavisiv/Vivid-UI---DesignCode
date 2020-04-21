dd3c2129bdef8e25a09887c96deef0a5f2c81d93
George Davis IV <GDavisIV@gmail.com>
2020-04-20 22:18:08 -0400

    Implementing a new BlurView

---
+++ DesignCode_1/BlurView.swift
@@ -0,0 +1,14 @@
+//
+//  BlurView.swift
+//  DesignCode_1
+//
+//  Created by George Davis IV on 4/20/20.
+//  Copyright © 2020 user163072. All rights reserved.
+//
+
+import SwiftUI
+
+//Going to create a 
+struct BlurView: UIViewRepresentable {
+    typlealias typealias UIViewType = <#T##Type###>
+}
