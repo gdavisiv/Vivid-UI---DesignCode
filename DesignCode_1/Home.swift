8b31ac9ca817a191bc4ff3711e4087cd237c8582
George Davis IV <GDavisIV@gmail.com>
2020-04-22 21:16:02 -0400

    Setting up Dark Mode

--- DesignCode_1/Home.swift
+++ DesignCode_1/Home.swift
@@ -116,7 +116,9 @@ struct Home: View {
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        //Commented out original Home to add in customization of app in dark mode
+        //Home()
+        Home().environment(\.colorScheme, .dark)
     }
 }
 
