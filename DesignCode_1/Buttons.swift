702701da68a5355b6326500f7f0e5041287d6e9b
George Davis IV <GDavisIV@gmail.com>
2020-04-26 17:07:28 -0400

    Finished Commenting for Neumorphic Button Design

--- DesignCode_1/Buttons.swift
+++ DesignCode_1/Buttons.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct Buttons: View {
     var body: some View {
+        //
         VStack {
             Text("Button")
                 .font(.system(size: 20, weight: .semibold, design: .rounded))
@@ -18,29 +19,37 @@ struct Buttons: View {
                     ZStack {
                         Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
                     
+                        //Creating a Second Color Layer with an inner shadow within the button
                         RoundedRectangle(cornerRadius: 16, style:
                             .continuous)
                             .foregroundColor(.white)
                             .blur(radius: 4)
                             .offset(x: -8, y: -8)
                         
+                        //Creating a third color layer
                         RoundedRectangle(cornerRadius: 16, style:
                         .continuous)
                         .fill(
-                            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                            //Adding a Linear gradient that runs from left to right and adds a nice transition between the two colors
+                            LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.8593714833, green: 0.87916857, blue: 0.9651435018, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
                         )
-                        //Adds a slight inner shadow while also
+                        //Adds a slight inner shadow while also adding blur
                         .padding(2)
                         .blur(radius: 2)
 
                         }
                 )
+                //Roundes the edges of the Rectangle
                 .clipShape(RoundedRectangle(cornerRadius: 60, style: .continuous))
+                //Creating two drop shadows one from top left, and one from the Bottom Right
                 .shadow(color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 20, x: 20, y: 20)
                 .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
         }
-        .frame(maxWidth: .init(), maxHeight: .infinity)
-        .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+        //
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //Setting the background color to color literal set below
+        .background(Color(#colorLiteral(red: 0.8933308721, green: 0.935842216, blue: 1, alpha: 1)))
+        //Going to ignore the edges and fill the entire screenw
         .edgesIgnoringSafeArea(.all)
     }
 }
