3d42129d40e6de21b0672d08faad229fb3d6eaa2
George Davis IV <GDavisIV@gmail.com>
2020-04-25 23:59:47 -0400

    New Revisions/Updates with Buttons

---
+++ DesignCode_1/Buttons.swift
@@ -0,0 +1,21 @@
+//
+//  Buttons.swift
+//  DesignCode_1
+//
+//  Created by George Davis IV on 4/25/20.
+//  Copyright © 2020 user163072. All rights reserved.
+//
+
+import SwiftUI
+
+struct Buttons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
