de4fda3c3aac266beb13ce65f346f385eae630aa
George Davis IV <GDavisIV@gmail.com>
2020-04-29 16:56:26 -0400

    Completed Neumorphic Circular Button

--- DesignCode_1/Buttons.swift
+++ DesignCode_1/Buttons.swift
@@ -134,21 +134,21 @@ struct CircleButton: View {
         .frame(width: 100, height: 100)
         .background(
             ZStack {
-                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)), Color(#colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1) : #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)), Color(press ? #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 //Gives a light grey stroke around the bottom right of the round button
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
-                    .shadow(color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 3, x: -5, y: -5)
+                    .shadow(color: Color(press ? #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1)), radius: 3, x: -5, y: -5)
                 //Gives a white stroke around the top Left corner of the round button
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
-                    .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 3, x: 3, y: 3)
+                    .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)), radius: 3, x: 3, y: 3)
             }
             
         )
             .clipShape(Circle())
-            .shadow(color: Color.white, radius: 20, x: -20, y: -20)
-            .shadow(color: Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 20, x: 20, y: 20)
+            .shadow(color: Color(press ? #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1) : #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)), radius: 20, x: -20, y: -20)
+            .shadow(color: Color(press ? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) : #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 20, x: 20, y: 20)
             //Use a scale effect to expand and normalize the size of the button as it is tapped
             .scaleEffect(tap ? 1.2 : 1)
             .gesture(
