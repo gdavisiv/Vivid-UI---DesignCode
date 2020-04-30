d9fe226d91027704c4fea7c6d19b8f8dc540e078
George Davis IV <GDavisIV@gmail.com>
2020-04-30 11:14:47 -0400

    Uploaded new Assets/Created new Tap Animation

--- DesignCode_1/Buttons.swift
+++ DesignCode_1/Buttons.swift
@@ -136,7 +136,7 @@ struct CircleButton: View {
         .frame(width: 100, height: 100)
         .background(
             ZStack {
-                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1) : #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)), Color(press ? #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)), Color(press ? #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 //Gives a light grey stroke around the bottom right of the round button
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
@@ -170,47 +170,40 @@ struct CircleButton: View {
 
 struct PayButton: View {
     //Create the states neede to Transition between
-    @State var tap = false
+    @GestureState var tap = false
     @State var press = false
     
     var body: some View {
         ZStack {
-            Image(systemName: "sun.max")
-                .font(.system(size: 44, weight: .light))
-                .offset(x: press ? -90 : 0, y: press ? -90 : 0)
-                .rotation3DEffect(Angle(degrees: press ? 20 : 0), axis: (x: 10, y: -8, z: 0))
-            Image(systemName: "moon")
-                .font(.system(size: 44, weight: .light))
-                .offset(x: press ? 0 : 90, y: press ? 0 : 90)
-                .rotation3DEffect(Angle(degrees: press ? 0 : 20), axis: (x: -10, y: 8, z: 0))
+            Image("fingerprint")
+            
+            Image("fingerprint-2")
+                .clipShape(Rectangle().offset(y: tap ? 0 : 50))
         }
-        .frame(width: 100, height: 100)
+        .frame(width: 120, height: 120)
         .background(
             ZStack {
-                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1) : #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)), Color(press ? #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1) : #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                LinearGradient(gradient: Gradient(colors: [Color(press ? #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(press ? #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1) : #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
                 //Gives a light grey stroke around the bottom right of the round button
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
-                    .shadow(color: Color(press ? #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1) : #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)), radius: 3, x: -5, y: -5)
+                    .shadow(color: Color(press ? #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1) : #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)), radius: 3, x: -5, y: -5)
                 //Gives a white stroke around the top Left corner of the round button
                 Circle()
                     .stroke(Color.clear, lineWidth: 10)
-                    .shadow(color: Color(press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.9686274529, green: 0.78039217, blue: 0.3450980484, alpha: 1)), radius: 3, x: 3, y: 3)
+                    .shadow(color: Color(press ? #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1) : #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 3, x: 3, y: 3)
             }
             
         )
             .clipShape(Circle())
-            .shadow(color: Color(press ? #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1) : #colorLiteral(red: 0.9764705896, green: 0.850980401, blue: 0.5490196347, alpha: 1)), radius: 20, x: -20, y: -20)
-            .shadow(color: Color(press ? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) : #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 20, x: 20, y: 20)
+            .shadow(color: Color(press ? #colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1) : #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)), radius: 20, x: -20, y: -20)
+            .shadow(color: Color(press ? #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1) : #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: 20, y: 20)
             //Use a scale effect to expand and normalize the size of the button as it is tapped
             .scaleEffect(tap ? 1.2 : 1)
             .gesture(
-                LongPressGesture().onChanged { value in
-                    self.tap = true
-                    //Make tap go back to false after a delay
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        self.tap = false
-                    }
+                LongPressGesture().updating($tap) { currentState, gestureState,
+                    transaction in
+                    gestureState = currentState
                 }
                 .onEnded { value in
                     self.press.toggle()
