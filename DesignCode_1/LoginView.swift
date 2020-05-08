8bcf806b72c058dc25cf61de9d5bae39807ef921
George Davis IV <GDavisIV@gmail.com>
2020-05-07 22:33:39 -0400

    Implementing User Login Info

--- DesignCode_1/LoginView.swift
+++ DesignCode_1/LoginView.swift
@@ -9,11 +9,10 @@
 import SwiftUI
 
 struct LoginView: View {
-    //Set the Animation State to False
-    @State var show = false
-    //Use for 3D Parallax
-    @State var viewState = CGSize.zero
-    @State var isDragging = false
+    //Create empty State
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         //This will only work to align the elements inside the Zstack; !against each other!
         ZStack(alignment: .top) {
@@ -25,26 +24,51 @@ struct LoginView: View {
                 //Clips the shape of the corners and rounds the top/bottom corners
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .edgesIgnoringSafeArea(.bottom)
-            VStack {
-                GeometryReader { geometry in
-                    Text("Learn, Design & Code.\nFrom scratch!")
-                        .font(.system(size: geometry.size.width/10.3, weight: .bold))
-                        //Makes text white
-                        .foregroundColor(.white)
-                }
-                .frame(maxWidth: 375, maxHeight: 100)
-                .padding(.horizontal, 16)
+            
+            CoverView()
+            
+            TextField("You Email", text: $email)
+            TextField("You Password", text: $email)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+        //Use to preivew on different devices
+        //.previewDevice("iPad Air 2")
+    }
+}
+
+struct CoverView: View {
+    //Set the Animation State to False
+    @State var show = false
+    //Use for 3D Parallax
+    @State var viewState = CGSize.zero
+    @State var isDragging = false
+    
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                Text("Learn, Design & Code.\nFrom scratch!")
+                    .font(.system(size: geometry.size.width/10.3, weight: .bold))
+                    //Makes text white
+                    .foregroundColor(.white)
+            }
+            .frame(maxWidth: 375, maxHeight: 100)
+            .padding(.horizontal, 16)
                 //Can also implement the parallax here divide it to slow down movement
                 .offset(x: viewState.width/18, y: viewState.height/18)
-                
-                Text("80 Hours of courses for SwiftUI, React and design tools.")
-                    .font(.subheadline)
-                    .frame(width: 250)
-                    //Can also implement the parallax here divide it to slow down movement
-                    .offset(x: viewState.width/23, y: viewState.height/23)
-                    
-                Spacer()
-            }
+            
+            Text("80 Hours of courses for SwiftUI, React and design tools.")
+                .font(.subheadline)
+                .frame(width: 250)
+                //Can also implement the parallax here divide it to slow down movement
+                .offset(x: viewState.width/23, y: viewState.height/23)
+            
+            Spacer()
+        }
             //Multiline will apply to all children in the VSTack and CENTER the text!
             .multilineTextAlignment(.center)
             //Padding will push down the text from the top in the VStack
@@ -75,13 +99,13 @@ struct LoginView: View {
                         .blendMode(.difference)
                         .animation(Animation.linear(duration: 100).repeatForever(autoreverses: true))
                 }
-            )
+        )
             //This adds the Image background
             //Implement the 3D Parallax Effect to the background as well!
             .background(
                 Image(uiImage: #imageLiteral(resourceName: "Card3")).offset(x: viewState.width/26, y: viewState.height/26),
                 alignment: .bottom
-            )
+        )
             //Background Color the forefront image
             .background(Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)))
             //Rounds the Edges
@@ -99,21 +123,12 @@ struct LoginView: View {
                     //Add a Scale effect
                     self.isDragging = true
                 }
-                //When the use takes their finger off the screen
-                .onEnded { value in
-                    //Reset the position
-                    self.viewState = .zero
-                    self.isDragging = false
+                    //When the use takes their finger off the screen
+                    .onEnded { value in
+                        //Reset the position
+                        self.viewState = .zero
+                        self.isDragging = false
                 }
-            )
-        }
-    }
-}
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
-        //Use to preivew on different devices
-        //.previewDevice("iPad Air 2")
+        )
     }
 }
