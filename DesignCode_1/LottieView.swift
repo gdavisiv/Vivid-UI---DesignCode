2c5d54cbcf2b8b41165315e865fc9d6493ada44a
George Davis IV <GDavisIV@gmail.com>
2020-05-09 13:22:37 -0400

    Created New LottieView File

---
+++ DesignCode_1/LottieView.swift
@@ -0,0 +1,42 @@
+//
+//  LottieView.swift
+//  DesignCode_1
+//
+//  Created by George Davis IV on 5/9/20.
+//  Copyright © 2020 user163072. All rights reserved.
+//
+
+import SwiftUI
+import Lottie
+
+//Going to create a UIView Representable, so we can use Normal Swift Code
+
+struct LottieView: UIViewRepresentable {
+    typealias UIViewType = UIView
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        let view = UIView(frame: .zero)
+        
+        //Create an animation view from the library
+        let animationView = AnimationView()
+        let animation = Animation.named("loading")
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.play()
+        
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+        
+    }
+}
